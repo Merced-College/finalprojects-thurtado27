6ef2d0a0cb4f64b429efa28ea84602751f2834d9
thurtado27 <155494987+thurtado27@users.noreply.github.com>
2026-04-16 01:25:28 +0000

    Created MenuItem and Menu class

---
+++ MenuItem.java
@@ -0,0 +1,17 @@
+/*
+Author: Trinity Hurtado
+Class: MenuItem.java
+
+Description:
+Represents a single menu item with name and price.
+*/
+
+public class MenuItem {
+    String name;
+    double price;
+
+    public MenuItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+}
